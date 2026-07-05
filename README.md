d44f36332d6b8698e193ed8a7f49e79f60c3dc1e
Prajna Niranjan Shetty <your-github-email@example.com>
2026-07-06 03:16:44 +0530

    model evaluation and mlflow added

--- README.md
+++ README.md
@@ -9,5 +9,79 @@
 6. Update the components
 7. Update the pipeline
 8. Update the main.py
-9. Update the dvc.yaml
+9. Update the app.py
 
+---
+
+# How to run?
+
+## STEPS
+
+Clone the repository
+
+```bash
+https://github.com/PrajnaShe/End-to-End-Machine-Learning-Project-with-ML-Flow
+```
+
+### STEP 01 - Create a conda environment after opening the repository
+
+```bash
+conda create -n mlproj python=3.8 -y
+```
+
+Activate the environment
+
+```bash
+conda activate mlproj
+```
+
+### STEP 02 - Install the requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+Finally run the application
+
+```bash
+python app.py
+```
+
+Open your localhost and port in the browser.
+
+---
+
+# MLflow
+
+[Documentation:](https://mlflow.org/docs/latest/index.html)
+
+Run the MLflow UI
+
+```bash
+mlflow ui
+```
+
+---
+
+# DagsHub
+
+https://dagshub.com/
+
+Run directly:
+
+```bash
+MLFLOW_TRACKING_URI=https://dagshub.com/PrajnaShe/End-to-End-Machine-Learning-Project-with-ML-Flow.mlflow \
+MLFLOW_TRACKING_USERNAME=PrajnaShe \
+MLFLOW_TRACKING_PASSWORD=3618e24fb2b028c441d90ae5b079ff8a2ce94970 \
+python script.py
+```
+
+Or export the variables permanently for the current terminal session:
+
+```bash
+export MLFLOW_TRACKING_URI=https://dagshub.com/PrajnaShe/End-to-End-Machine-Learning-Project-with-ML-Flow.mlflow
+
+export MLFLOW_TRACKING_USERNAME=PrajnaShe
+
+export MLFLOW_TRACKING_PASSWORD=3618e24fb2b028c441d90ae5b079ff8a2ce94970
+```
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
@@ -18,8 +18,8 @@ except Exception as e:
     raise e
 
 
-STAGE_NAME = "Data Validation stage"
 
+STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
@@ -30,8 +30,8 @@ except Exception as e:
     raise e
 
 
-STAGE_NAME = "Data Transformation stage"
 
+STAGE_NAME = "Data Transformation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
@@ -41,15 +41,29 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-STAGE_NAME = "Model Trainer stage"
 
+
+STAGE_NAME = "Model Trainer stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = ModelTrainerTrainingPipeline()
     data_ingestion.main()
     logger.info(
-        f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x"
-    )
+        f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
+    data_ingestion = ModelEvaluationTrainingPipeline()
+    data_ingestion.main()
+
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
